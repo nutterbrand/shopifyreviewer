d4f94e10687113b3ef9705266ecb5c432cb441bb
Henry Bi <henry@clearvoice.com>
2020-04-22 22:25:16 -0700

    Reformat

--- pages/blindsgalore.js
+++ pages/blindsgalore.js
@@ -1,16 +1,17 @@
-import React, { useState } from "react";
-import classNames from "classnames";
-import { makeStyles } from "@material-ui/core/styles";
-import Rating from "@material-ui/lab/Rating";
-import Button from "@material-ui/core/Button";
-import Header from "components/Header/Header.js";
-import GridContainer from "components/Grid/GridContainer.js";
-import GridItem from "components/Grid/GridItem.js";
-import CompanyLogo from "assets/img/blindsgalore.jpg";
-import fetch from "isomorphic-unfetch";
-import useSwr from "swr";
-import _ from "underscore";
-import productStyle from "assets/jss/nextjs-material-kit-pro/pages/productStyle.js";
+import React, {useState} from 'react';
+import classNames from 'classnames';
+import {makeStyles} from '@material-ui/core/styles';
+import Rating from '@material-ui/lab/Rating';
+import Button from '@material-ui/core/Button';
+import Header from 'components/Header/Header.js';
+import GridContainer from 'components/Grid/GridContainer.js';
+import GridItem from 'components/Grid/GridItem.js';
+import CompanyLogo from 'assets/img/blindsgalore.jpg';
+import fetch from 'isomorphic-unfetch';
+import useSwr from 'swr';
+import _ from 'underscore';
+import productStyle from 'assets/jss/nextjs-material-kit-pro/pages/productStyle.js';
+
 const useStyles = makeStyles(productStyle);
 
 const fetcher = url => fetch(url).then((res) => res.json());
@@ -18,10 +19,10 @@ const fetcher = url => fetch(url).then((res) => res.json());
 export default function BlindsgalorePage(props) {
   const [isSignedUp, updateStatus] = useState(false);
   const classes = useStyles();
-  const { data, error } = useSwr("/api/shopping", fetcher);
+  const {data, error} = useSwr('/api/shopping', fetcher);
   if (error) return <div>Failed to load data</div>;
   if (!data) return <div>Loading...</div>;
-  const groupedData = _.filter(_.groupBy(data, "keyword"), product => product.length > 3);
+  const groupedData = _.filter(_.groupBy(data, 'keyword'), product => product.length > 3);
   const productsData = _.keys(groupedData);
   const getSuggestions = (data, keyword) => {
     const prices = _(data).pluck('extracted_price');
@@ -67,19 +68,21 @@ export default function BlindsgalorePage(props) {
       ];
     }
     return suggestions;
-  }
+  };
 
   return (
-    <div className={classes.productPage}>
-      <Header fixed color="white" brand="Shopify Reviewer"
-              links={<Button color={isSignedUp ? "primary" : "secondary"} variant="contained" className={classes.signUp}
-                             onClick={()=>updateStatus(!isSignedUp)}>{!isSignedUp ? "Sign Up" : "Account"}</Button>}/>
-      <div className={classNames(classes.section)}>
-        <div className={classes.container}>
-          <GridContainer className={classes.productHeader}>
-            <GridItem md={7} sm={12}>
-              <img className={classes.headerImg} src={CompanyLogo} />
-              <h3>Shopping Score Overview:</h3>
+      <div className={classes.productPage}>
+        <Header fixed color="white" brand="Shopify Reviewer"
+                links={
+                  <Button color={isSignedUp ? 'primary' : 'secondary'} variant="contained"
+                          className={classes.signUp} onClick={() => updateStatus(!isSignedUp)}>
+                    {!isSignedUp ? 'Sign Up' : 'Account'}</Button>}/>
+        <div className={classNames(classes.section)}>
+          <div className={classes.container}>
+            <GridContainer className={classes.productHeader}>
+              <GridItem md={7} sm={12}>
+                <img className={classes.headerImg} src={CompanyLogo}/>
+                <h3>Shopping Score Overview:</h3>
                 <div className={classes.stat}>
                   <b className={classes.statNum}>23%</b> of the time your company
                   shows up for the top keywords in your industry.
@@ -92,59 +95,60 @@ export default function BlindsgalorePage(props) {
                   <b className={classes.statNum}>4%</b> of the time your company
                   ratings on products for top keywords.
                 </div>
-            </GridItem>
-            <GridItem md={5} sm={12}>
-              <h2>Score: <b className={classes.statNum}>12%</b></h2>
-              <p>We did a full on analysis on over a 100 of the most
-                popular searches that bring customers to your site.</p>
-              <p>We found quite a few areas we you can improve your
-                Google advertising to get more customers buying on
-                your site.</p>
-            </GridItem>
-          </GridContainer>
-          {productsData.map( (key, keyIndex) => {
-              const products = groupedData[key];
-              const keyword = products[0].keyword;
+              </GridItem>
+              <GridItem md={5} sm={12}>
+                <h2>Score: <b className={classes.statNum}>12%</b></h2>
+                <p>We did a full on analysis on over a 100 of the most
+                  popular searches that bring customers to your site.</p>
+                <p>We found quite a few areas we you can improve your
+                  Google advertising to get more customers buying on
+                  your site.</p>
+              </GridItem>
+            </GridContainer>
+            {productsData.map((key, keyIndex) => {
+              const products = groupedData[ key ];
+              const keyword = products[ 0 ].keyword;
               const suggestions = getSuggestions(products, keyword);
               const shouldBlurClass = {[ classes.blur ]: keyIndex > 9 && !isSignedUp};
               return <div className={classes.keywordHeader}>
-                        <GridContainer>
-                          <GridItem md={12} sm={12} className={classes.suggestionsContainer}>
-                            <h3 className={classes.keywordTitle}>Keyword: "{keyword}"</h3>
-                            <h4 className={classNames(shouldBlurClass)}> Suggestions: </h4>
-                              {suggestions.map((suggestion,i) => {
-                                return <div key={i} className={classNames(shouldBlurClass)}>{suggestion}</div>;
-                              })}
-                          </GridItem>
-                          <GridItem md={12} sm={12} className={classNames(shouldBlurClass)}>
-                          <GridContainer className={classes.productRow}>
-                          {products.map( (product, index) => {
-                            return (
-                                <div key={index} className={classes.productContainer}>
-                                  <img src={product.thumbnail} className={classNames(classes.productImg, shouldBlurClass)}/>
-                                  <div>
-                                    <div className={classes.productKeyword}>
-                                      <b>#{index + 1}: {product.keyword}</b>
-                                    </div>
-                                    <div className={classes.productLink}>{product.source}</div>
-                                    <div className={classes.dollar}>${product.extracted_price}</div>
-                                    <div className={classes.description}>
-                                      <Rating name="read-only" value={parseInt(product.rating)} precision={0.5} size="small" readOnly/>
-                                      <div>{product.title}</div>
-                                    </div>
-                                  </div>
+                <GridContainer>
+                  <GridItem md={12} sm={12} className={classes.suggestionsContainer}>
+                    <h3 className={classes.keywordTitle}>Keyword: "{keyword}"</h3>
+                    <h4 className={classNames(shouldBlurClass)}> Suggestions: </h4>
+                    {suggestions.map((suggestion, i) => {
+                      return <div key={i} className={classNames(shouldBlurClass)}>{suggestion}</div>;
+                    })}
+                  </GridItem>
+                  <GridItem md={12} sm={12} className={classNames(shouldBlurClass)}>
+                    <GridContainer className={classes.productRow}>
+                      {products.map((product, index) => {
+                        return (
+                            <div key={index} className={classes.productContainer}>
+                              <img src={product.thumbnail} className={classNames(classes.productImg, shouldBlurClass)}/>
+                              <div>
+                                <div className={classes.productKeyword}>
+                                  <b>#{index + 1}: {product.keyword}</b>
                                 </div>
-                              );
-                          })}
-                            {products.length > 5 &&
-                            <i className={classes.more}>There's {products.length - 5} more products, scroll right...</i>}
-                          </GridContainer>
-                          </GridItem>
-                        </GridContainer>
-                    </div>
+                                <div className={classes.productLink}>{product.source}</div>
+                                <div className={classes.dollar}>${product.extracted_price}</div>
+                                <div className={classes.description}>
+                                  <Rating name="read-only" value={parseInt(product.rating)} precision={0.5} size="small"
+                                          readOnly/>
+                                  <div>{product.title}</div>
+                                </div>
+                              </div>
+                            </div>
+                        );
+                      })}
+                      {products.length > 5 &&
+                      <i className={classes.more}>There's {products.length - 5} more products, scroll right...</i>}
+                    </GridContainer>
+                  </GridItem>
+                </GridContainer>
+              </div>;
             })}
+          </div>
         </div>
       </div>
-    </div>
   );
 }
