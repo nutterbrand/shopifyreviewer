7399203a72381fa45a1b7256403f849b4aa4bc8a
Henry Bi <henrybidesign@gmail.com>
2020-05-17 23:33:45 -0700

    Add homepage rout and tabs for keywords

--- pages/blindsgalore.js
+++ pages/blindsgalore.js
@@ -1,64 +1,24 @@
+
 import React, {useState} from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import _ from 'underscore';
 import CompanyLogo from 'assets/img/blindsgalore.jpg';
 import fetch from 'isomorphic-unfetch';
 import useSwr from 'swr';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
 import productStyle from 'assets/jss/nextjs-material-kit-pro/pages/productStyle.js';
 import {CompanyScoreCard} from './CompanyScoreCard.js';
 import {Header} from './Header';
 import {KeywordProducts} from './KeywordProducts';
 import 'react-circular-progressbar/dist/styles.css';
-import GridContainer from '../components/Grid/GridContainer';
-import GridItem from '../components/Grid/GridItem';
-import Button from '@material-ui/core/Button';
-import classNames from 'classnames';
-import {Recommendations} from './Recommendations';
-import GoogleSearch from '../assets/img/googleSearch.png';
-import {Product} from './Product';
 
 const useStyles = makeStyles(productStyle);
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
-
-const TabPanel = props => {
-  const { children, value, index, ...other } = props;
-  return (
-      <div
-          role="tabpanel"
-          hidden={value !== index}
-          id={`simple-tabpanel-${index}`}
-          aria-labelledby={`simple-tab-${index}`}
-          {...other}
-      >
-        {value === index && (
-            <Box p={3}>
-              <Typography>{children}</Typography>
-            </Box>
-        )}
-      </div>
-  );
-}
-
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
-}
-
-
 export default function BlindsgalorePage(props) {
   const [isSignedUp, updateStatus] = useState(false);
   const classes = useStyles();
-  const {data, error} = useSwr('https://evening-retreat-22032.herokuapp.com/report/blindsgalore.com/', fetcher);
+  const {data, error} = useSwr('/api/get/shopping/blindsgalore.com', fetcher);
   if (error) return <span className={classes.center}>Failed to load data</span>;
-  console.log(data);
   if (!data)
     return (
         <div className={classes.center}>
@@ -78,57 +38,15 @@ export default function BlindsgalorePage(props) {
           <div className={classes.section}>
             <div className={classes.container}>
               <CompanyScoreCard/>
-              {/*{productsData.map((key, keyIndex) => (*/}
-              {/*    <KeywordProducts*/}
-              {/*        key={key}*/}
-              {/*        keyIndex={keyIndex}*/}
-              {/*        products={groupedData[ key ]}*/}
-              {/*        isSignedUp={isSignedUp}*/}
-              {/*        updateStatus={updateStatus}*/}
-              {/*    />*/}
-              {/*))}*/}
-              {
-                data.result.map(r => {
-                  return <div className={classes.keywordCard}>
-                    <GridContainer>
-                      <GridItem md={12} sm={12} className={classes.recsContainer}>
-                        <h3 className={classes.keywordTitle}>#{r.position} Keyword: "{r.keyword}"</h3>
-                        <div className={classNames(classes.searchContainer)}>
-                          <h4 className={classes.searchingResult}>{r.keyword}</h4>
-                          <img className={classes.googleSearch} src={GoogleSearch}/>
-                        </div>
-                      </GridItem>
-                      {/*<AppBar position="static">*/}
-                      {/*  <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">*/}
-                      {/*    <Tab label="Item One" {...a11yProps(0)} />*/}
-                      {/*    <Tab label="Item Two" {...a11yProps(1)} />*/}
-                      {/*    <Tab label="Item Three" {...a11yProps(2)} />*/}
-                      {/*  </Tabs>*/}
-                      {/*</AppBar>*/}
-                      {/*<TabPanel value={value} index={0}>*/}
-                      {/*  Item One*/}
-                      {/*</TabPanel>*/}
-                      {/*<TabPanel value={value} index={1}>*/}
-                      {/*  Item Two*/}
-                      {/*</TabPanel>*/}
-                      {/*<TabPanel value={value} index={2}>*/}
-                      {/*  Item Three*/}
-                      {/*</TabPanel>*/}
-                      {/*<GridItem md={12} sm={12} className={classNames(blurClass)}>*/}
-                      {/*  <GridContainer className={classes.productRow}>*/}
-                      {/*    {products.map((product, index) =>*/}
-                      {/*        <Product key={index} index={index} product={product} yourCompany={yourCompany}*/}
-                      {/*                 blurClass={blurClass}/>,*/}
-                      {/*    )}*/}
-                      {/*    {products.length > 5 &&*/}
-                      {/*    <i className={classes.more}>There's {products.length - 5} more products, scroll*/}
-                      {/*      right...</i>}*/}
-                      {/*  </GridContainer>*/}
-                      {/*</GridItem>*/}
-                    </GridContainer>
-                  </div>;
-                })
-              }
+              {productsData.map((key, keyIndex) => (
+                  <KeywordProducts
+                      key={key}
+                      keyIndex={keyIndex}
+                      products={groupedData[ key ]}
+                      isSignedUp={isSignedUp}
+                      updateStatus={updateStatus}
+                  />
+              ))}
             </div>
           </div>
         </div>
