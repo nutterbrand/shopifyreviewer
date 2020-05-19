bb73c8d2e80d15892698d1ba3d657b2c4aaa75ae
Henry Bi <henrybidesign@gmail.com>
2020-05-18 19:56:44 -0700

    Refactor TabPanel

--- pages/TabPanel.js
+++ pages/TabPanel.js
@@ -5,18 +5,10 @@ import Box from '@material-ui/core/Box';
 export const TabPanel = props => {
   const {children, value, index, ...other} = props;
   return (
-      <div
-          role="tabpanel"
-          hidden={value !== index}
-          id={`simple-tabpanel-${index}`}
-          aria-labelledby={`simple-tab-${index}`}
-          {...other}
-      >
-        {value === index && (
-            <Box p={3}>
-              <Typography>{children}</Typography>
-            </Box>
-        )}
+      <div role="tabpanel" hidden={value !== index} id={`simple-tabpanel-${index}`}
+           aria-labelledby={`simple-tab-${index}`}
+           {...other}>
+        {value === index && <Box p={3}><Typography>{children}</Typography></Box>}
       </div>
   );
 };
