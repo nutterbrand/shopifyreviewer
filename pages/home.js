ad83ef8244215005dee87d281d14ebd15d05d9e7
Henry Bi <henrybidesign@gmail.com>
2020-06-28 17:53:50 -0700

    Update home page with new route

--- pages/home.js
+++ pages/home.js
@@ -4,33 +4,44 @@ import _ from 'underscore';
 import {makeStyles} from '@material-ui/core/styles';
 import fetch from 'isomorphic-unfetch';
 import useSwr from 'swr';
-import productStyle from 'assets/jss/nextjs-material-kit-pro/pages/productStyle.js';
-import {CompanyScoreCard} from './CompanyScoreCard.js';
 import {Header} from './Header';
-import {SimpleLoading} from './SimpleLoading';
+import {CompanyHeader} from './CompanyHeader';
+import {LoadingSpinner} from './LoadingSpinner';
 import {KeywordGroup} from './KeywordGroup';
-import {LoadingPlaceholder} from './LoadingPlaceholder';
-import 'react-circular-progressbar/dist/styles.css';
+import productStyle from 'assets/jss/nextjs-material-kit-pro/pages/productStyle.js';
 
 const useStyles = makeStyles(productStyle);
-const fetcher = (url) => fetch(url).then((res) => res.json());
+const fetcher = url => fetch(url).then((res) => res.json());
 
 export default function HomePage() {
   const classes = useStyles();
   const router = useRouter();
-  const showAll = _.has(router.query, 'show');
-  const {data, error} = useSwr('https://evening-retreat-22032.herokuapp.com/report/blindsgalore.com/', fetcher);
-  // const {data, error} = useSwr('https://evening-retreat-22032.herokuapp.com/report/signaturehardware.com/', fetcher);
+  const showAll
+      = _.has(router.query, 'show');
+  const {data, error} = useSwr('https://evening-retreat-22032.herokuapp.com/domain/woodencork.com', fetcher);
   if (error) return <span className={classes.center}>Failed to load data</span>;
-  if (!data) return <SimpleLoading/>;
-  console.log(data.result);
+  if (!data) return <LoadingSpinner
+      loadingImages={
+        [
+          'https://henrymetricstory.s3-us-west-1.amazonaws.com/load_1.png',
+          'https://henrymetricstory.s3-us-west-1.amazonaws.com/load_2.png',
+          'https://henrymetricstory.s3-us-west-1.amazonaws.com/load_3.png',
+        ]
+      }
+      loadingMessages={[
+        'Downloading Amazon Product Page...',
+        'Analyzing Competitors...',
+        'Building Landing Page...',
+      ]}
+  />;
+  // console.log(data.result);
   return (
       <>
         <Header/>
         <div className={classes.companyPage}>
           <div className={classes.section}>
             <div className={classes.container}>
-              <CompanyScoreCard/>
+              <CompanyHeader/>
               {
                 data.result.map(
                     (result, i) => <KeywordGroup result={result} key={result.keyword} index={i} showAll={showAll}/>)
