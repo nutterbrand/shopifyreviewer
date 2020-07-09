66f52b0ee359c16bbc21edd40b34843c2645e3cf
Henry Bi <henrybidesign@gmail.com>
2020-07-08 20:12:47 -0700

    Fix error in console with Typography component

--- components/Project/TabPanel.js
+++ components/Project/TabPanel.js
@@ -1,27 +1,27 @@
-import React from "react";
-import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
-import { makeStyles } from "@material-ui/core/styles";
-import productStyle from "../../assets/jss/nextjs-material-kit-pro/pages/productStyle";
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import {makeStyles} from '@material-ui/core/styles';
+import productStyle from '../../assets/jss/nextjs-material-kit-pro/pages/productStyle';
 
 const useStyles = makeStyles(productStyle);
 
 export const TabPanel = (props) => {
   const classes = useStyles();
-  const { children, value, index, ...other } = props;
+  const {children, value, index, ...other} = props;
   return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box className={classes.tabPanel} p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
+      <div
+          role="tabpanel"
+          hidden={value !== index}
+          id={`simple-tabpanel-${index}`}
+          aria-labelledby={`simple-tab-${index}`}
+          {...other}
+      >
+        {value === index && (
+            <Box className={classes.tabPanel} p={3}>
+              <Typography component={'span'}>{children}</Typography>
+            </Box>
+        )}
+      </div>
   );
 };
