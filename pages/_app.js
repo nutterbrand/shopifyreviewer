21b0e3df746ca1380b9a2b3bc8adf658868e41a6
Henry Bi <henrybidesign@gmail.com>
2020-09-02 21:11:46 -0700

    Add Auth0 provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import App from 'next/app';
+import {Auth0Provider} from '@auth0/auth0-react';
 import 'assets/scss/nextjs-material-kit-pro.scss?v=1.0.0';
 
 export default class MyApp extends App {
@@ -7,9 +8,13 @@ export default class MyApp extends App {
     const {Component, pageProps} = this.props;
 
     return (
-        <React.Fragment>
+        <Auth0Provider
+            domain="keywordshere.auth0.com"
+            clientId="JAPhwiupGDRBFmxud2NKLHqP4oX1nuDD"
+            redirectUri='http://localhost:8080'
+        >
           <Component {...pageProps} />
-        </React.Fragment>
+        </Auth0Provider>
     );
   }
 }
