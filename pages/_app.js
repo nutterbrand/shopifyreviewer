899a30cae1e10af2211fac740b594c54de2e2815
Henry Bi <henrybidesign@gmail.com>
2020-09-03 14:24:33 -0700

    Update redirect url logic for prod vs dev

--- pages/_app.js
+++ pages/_app.js
@@ -6,12 +6,15 @@ import 'assets/scss/nextjs-material-kit-pro.scss?v=1.0.0';
 export default class MyApp extends App {
   render() {
     const {Component, pageProps} = this.props;
-    const isProd = typeof window === 'undefined';
+    let redirectUrl = 'https://shopifyreviewer.com';
+    if (typeof window !== 'undefined') {
+      redirectUrl = window.location.origin;
+    }
     return (
         <Auth0Provider
             domain="keywordshere.auth0.com"
             clientId="JAPhwiupGDRBFmxud2NKLHqP4oX1nuDD"
-            redirectUri={isProd ? 'https://shopifyreviewer.com' : 'http://localhost:8080'}
+            redirectUri={redirectUrl}
         >
           <Component {...pageProps} />
         </Auth0Provider>
