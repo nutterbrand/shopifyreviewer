2cd133599941c5d3aff357bbef3767795a37e549
Henry Bi <henrybidesign@gmail.com>
2020-09-22 20:36:47 -0700

    Add useStickyState local storage helper method

--- components/Project/helpers/helper.js
+++ components/Project/helpers/helper.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export const filterDomain = domain => domain.replace(/^(?:https?:\/\/)?(?:www\.)?/i, '').split('/')[ 0 ];
 
 export const convertArrayToObject = (array, key) => {
@@ -5,7 +7,22 @@ export const convertArrayToObject = (array, key) => {
   return array.reduce((obj, item) => {
     return {
       ...obj,
-      [item[key]]: item,
+      [ item[ key ] ]: item,
     };
   }, initialValue);
+};
+
+export const useStickyState = (defaultValue, key) => {
+  const [value, setValue] = React.useState(() => {
+    if (typeof window !== 'undefined') {
+      const stickyValue = window.localStorage.getItem(key);
+      return stickyValue !== null
+          ? JSON.parse(stickyValue)
+          : defaultValue;
+    }
+  });
+  React.useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+  return [value, setValue];
 };
