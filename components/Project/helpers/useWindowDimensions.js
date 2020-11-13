07420ddbb2296f1212129ea1ed381f375ca4fc7c
Henry Bi <henrybidesign@gmail.com>
2020-11-12 20:16:48 -0800

    Fix window undefined error

--- components/Project/helpers/useWindowDimensions.js
+++ components/Project/helpers/useWindowDimensions.js
@@ -1,16 +1,13 @@
-import { useState, useEffect } from 'react';
+import {useState, useEffect} from 'react';
 
 function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window;
-  return {
-    width,
-    height
-  };
+  const {innerWidth: width, innerHeight: height} = window;
+  return {width, height};
 }
 
 export default function useWindowDimensions() {
+  if (typeof window === 'undefined') return {width: 0, height: 0};
   const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-
   useEffect(() => {
     function handleResize() {
       setWindowDimensions(getWindowDimensions());
